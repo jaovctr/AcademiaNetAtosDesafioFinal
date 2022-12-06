7ee88f9c312a91893600538f7350bb4e5698c45e
João Victor <jvejv2011@gmail.com>
2022-12-06 18:51:31 -0300

    Banco de dados com Entity, parte inicial das Apis (não funcional)

---
+++ BuscoBicoBackEnd/Models/Cliente.cs
@@ -0,0 +1,12 @@
+﻿namespace BuscoBicoBackEnd.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string Localizacao { get; set; }
+        public virtual ICollection<Review>? Reviews { get; set; }
+
+    }
+}
